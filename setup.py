4fffff89accc13db54d980111aaaab4e233dae42
Tran Minh Tien <tmt31032005@gmail.com>
2025-08-12 12:11:39 +0700

    update file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Question Creator",
+    version= '0.0.0',
+    author="Tien Tran Minh",
+    author_email="tmt31032005@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
